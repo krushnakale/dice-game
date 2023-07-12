f330edfc81aa411563a1d2964a644f934449d6d5
krushna <krishnakale8565@gmail.com>
2023-07-12 16:19:21 +0530

    dice game

--- dice.js
+++ dice.js
@@ -5,8 +5,17 @@
    
 
 // }
-let container = document.querySelector('.dice_number_container');
+// let container = document.querySelector('.dice_number_container');
+// function playGame(){
+//    let randomNumber = Math.floor(Math.random()*10)+1;
+//     container.innerText=randomNumber;
+// }
+
 function playGame(){
-   let randomNumber = Math.floor(Math.random()*10)+1;
-    container.innerText=randomNumber;
-}
+    document.getElementById("container").innerHTML = Math.floor(Math.random()*6)+1;
+
+} l
+
+// let a = "ten" + 10;
+// let b = "ten" - 10;
+// console.log(a,b);
